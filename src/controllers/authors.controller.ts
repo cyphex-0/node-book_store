db8bdf67bc675340886ca1804db882055636c783
Shah Makhdum <contact.makhdum@gmail.com>
2026-05-17 00:55:21 +0600

    add author delete

--- src/controllers/authors.controller.ts
+++ src/controllers/authors.controller.ts
@@ -40,3 +40,21 @@ export const createAuthor = async (
     });
   return res.status(201).json({ Message: "Author register ", id: newAuthor!.id });
 };
+
+export const deleteAuthor = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+    const authorID = req.params.id as string;
+      const isPresent = await db
+        .select()
+        .from(authorTable)
+        .where(eq(authorTable.id, authorID))
+        .limit(1);
+      if (isPresent.length > 0) {
+        return res.status(404).json({ Error: `id ${authorID} not found` });
+      }
+      await db.delete(authorTable).where(eq(authorTable.id, authorID));
+      return res.status(200).json({ Success: `Author deleted` });
+}
--- src/routes/authors.route.ts
+++ src/routes/authors.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAuthor, createAuthor } from "../controllers/authors.controller";
+import { getAuthor, createAuthor, deleteAuthor } from "../controllers/authors.controller";
 const router = express.Router();
 
 router.get("/", getAuthor);
 router.post("/", createAuthor)
+router.delete("/:id", deleteAuthor)
 
 
 export default router
